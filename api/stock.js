c859fb8665020d88c11d318a949d6f30b0568a2d
Jhen-FM <70109029+Jhen-FM@users.noreply.github.com>
2026-04-18 17:13:01 -0300

    Update stock.js

--- api/stock.js
+++ api/stock.js
@@ -1,10 +1,15 @@
-import db from './db.js';
+import dbPromise from './db.js';
 
-export default function handler(req, res) {
-  const stock = db.prepare(`
+export default async function handler(req, res) {
+  const db = await dbPromise;
+  const stmt = db.prepare(`
     SELECT items.name, stock.qty
     FROM stock
     JOIN items ON stock.item_id = items.id
-  `).all();
-  res.status(200).json(stock);
+  `);
+  const result = [];
+  while (stmt.step()) {
+    result.push(stmt.getAsObject());
+  }
+  res.status(200).json(result);
 }
